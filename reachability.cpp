8215f44ce3027755e7a512ac322a4cf5347a4756
Kartik Kumar <Kartikkonline@gmail.com>
2020-10-09 01:42:53 +0530

    Create reachability.cpp
    A maze is a rectangular grid of cells with walls between some of adjacent cells.
    You would like to check whether there is a path from a given cell to a given
    exit from a maze where an exit is also a cell that lies on the border of the maze
    (in the example shown to the right there are two exits: one on the left border
    and one on the right border). For this, you represent the maze as an undirected
    graph: vertices of the graph are cells of the maze, two vertices are connected by
    an undirected edge if they are adjacent and there is no wall between them. Then,
    to check whether there is a path between two given cells in the maze, it suffices to
    check that there is a path between the corresponding two vertices in the graph.

---
+++ reachability.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::pair;
+
+vector < bool > explored;
+
+void Explore(vector<vector<int> > &adj, int v)
+{
+	explored[v] = true;
+	vector<int> &temp = (adj[v]);
+	for (int i = 0; i < temp.size(); ++i)
+	{
+		if (explored[temp[i]] != true)
+			Explore(adj,temp[i]);
+	}
+}
+
+int reach(vector<vector<int> > &adj, int x, int y)
+{
+	//write your code here
+
+	vector<int> &temp = (adj[x]);
+	/*for (int j = 0; j < adj.size(); j++)
+	{
+		explored
+	}
+*/
+	Explore(adj, x);
+	if (explored[y] == true)
+		return 1;
+	else
+		return 0;
+	/*for (int i = 0; i < temp.size(); i++)
+	{
+		explored[i] = true;
+		if (temp[i] == y)
+		{
+			return 1;
+		}
+	}*/
+	//std::cout << "0";
+	//return;
+	//return 0;
+}
+
+int main()
+{
+	size_t n, m;
+	std::cin >> n >> m;
+	vector<vector<int> > adj(n, vector<int>());
+	explored.assign(n, false);
+	for (size_t i = 0; i < m; i++)
+	{
+		int x, y;
+		std::cin >> x >> y;
+		adj[x - 1].push_back(y - 1);
+		adj[y - 1].push_back(x - 1);
+	}
+	int x, y;
+	std::cin >> x >> y;
+	std::cout << reach(adj, x - 1, y - 1);
+}
